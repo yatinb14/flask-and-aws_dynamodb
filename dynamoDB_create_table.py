a0c49a5b718969327b54a98cd9cc20db5ccffd5e
DELL <yatin.bhatia@thinknyx.com>
2021-05-26 13:22:58 +0530

    adding code

---
+++ dynamoDB_create_table.py
@@ -0,0 +1,44 @@
+
+
+import boto3
+# Get the service resource.
+import key_config as keys
+
+dynamodb = boto3.resource('dynamodb',
+                    aws_access_key_id=keys.ACCESS_KEY_ID,
+                    aws_secret_access_key=keys.ACCESS_SECRET_KEY)
+
+#dynamodb = boto3.resource('dynamodb')
+
+# Create the DynamoDB table.
+table = dynamodb.create_table(
+    TableName='userdata',
+    KeySchema=[
+        {
+            'AttributeName': 'email',
+            'KeyType': 'HASH'
+        }
+         
+    ],
+    AttributeDefinitions=[
+             {
+            'AttributeName': 'email',
+            'AttributeType': 'S'
+        } 
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+# Wait until the table exists.
+table.meta.client.get_waiter('table_exists').wait(TableName='userdata')
+
+# Print out some data about the table.
+print(table.item_count)
+
+
+
+
+ 
